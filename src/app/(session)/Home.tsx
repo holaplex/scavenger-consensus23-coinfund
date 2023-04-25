76b384f80753c7c6822d4a5140260737670a2103
Mackenzie Wildman <mackenzie.wildman@gmail.com>
2023-04-25 16:04:55 -0700

    src/app/(session)/Home.tsx

--- src/app/(session)/Home.tsx
+++ src/app/(session)/Home.tsx
@@ -46,14 +46,10 @@ export default function Home({ session }: HomeProps) {
     <>
       <img src="/img/hero.png" className="w-36 aspect-square object-cover" />
       <h1 className="text-2xl text-white mt-12 text-center">
-        CoinFund @ Consensus: Scavenger Hunt
+        CoinFund @ Consensus: Portco Scavenger Hunt
       </h1>
       <p className="text-gray-300 my-6 text-center">
-        <ul>
-          <li>Meet and connect with Coinfund Portfolio Companies</li>
-          <li>Mint their NFT only after you've done so</li>
-          <li>Collect 5 NFTs to be entered into our raffle for a Claynosaurz!</li>
-        </ul>
+        Collect 5 NFTs to be entered into our raffle for a Claynosaurz!
       </p>
       <div className="w-full grid grid-cols-4 gap-4">
         {loading ? (
